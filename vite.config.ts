af79cd9849aa280c947b6580ab93fe8e508500c1
lierrecheng-stack <lierrecheng@gmail.com>
2026-03-20 23:10:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
-
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/', 
+
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -16,12 +13,12 @@ export default defineConfig(({mode}) => {
       },
     },
     server: {
-  host: true,
-  port: 5173,
-  strictPort: true,
-  hmr: {
-    clientPort: 5173
+      host: true,
+      port: 5173,
+      strictPort: true,
+      hmr: {
+        clientPort: 5173
+      }
+    }
   }
-}
-}
 });
